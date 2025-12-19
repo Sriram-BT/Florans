fed94ce8a60119b11f97bcb248d628a2f3cfc5ba
Simon <simonraj7343@gmail.com>
2025-12-19 12:30:03 +0530

    Change the footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,27 @@
 import { Routes, Route } from "react-router-dom";
+
+import Header from "./Components/header";
+import Home from "./Components/home";
 import Login from "./Components/login";
 import Register from "./Components/register";
-import Home from "./Components/home";
-<<<<<<< HEAD
 import Supporters from "./Components/Supporters";
-
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/supporters" element={<Supporters />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-=======
 import ProjectsDone from "./Components/projectsDone";
-import Header from "./Components/header";
+import Footer from "./Components/footer"
 
 export default function App() {
   return (
     <>
-      {/* Header common to all pages */}
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/supporters" element={<Supporters />} />
+        <Route path="/programs" element={<ProjectsDone />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/programs" element={<ProjectsDone />} />
       </Routes>
+            <Footer />
+
     </>
->>>>>>> main
   );
 }
-
--- frontend/src/Components/home.jsx
+++ frontend/src/Components/home.jsx
@@ -8,12 +8,10 @@ import ProgramsSection from "./programs";
 export default function Home() {
   return (
     <div className="home-page">
-      <Header />
       <FounderSection />
       <LatestUpdates />
       <ProgramsSection />
       {/* <ProjectsDone /> */}
-      <Footer />
     </div>
   );
 }
