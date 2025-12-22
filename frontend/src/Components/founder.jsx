ff7f0ffaf983983f9f1868be85a3400acbe09c5e
Sriram <bt.sriram2343@gmail.com>
2025-12-22 11:25:46 +0530

    Added images

--- frontend/src/Components/founder.jsx
+++ frontend/src/Components/founder.jsx
@@ -1,6 +1,6 @@
 import "../Styles/FounderSection.css";
 import founderImg from '../Assets/founderImg.png'
-import jo from '../Assets/Jo.jpg'
+// import jo from '../Assets/Jo.jpg'
 
 export default function FounderSection() {
   return (
@@ -23,7 +23,7 @@ export default function FounderSection() {
       </div>
 
       <div className="founder-right">
-        <img src={jo} alt="Founder" className="founder-image" />
+        <img src={founderImg} alt="Founder" className="founder-image" />
       </div>
     </section>
   );
